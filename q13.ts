a5bb247655222bbe6ab18cbf0cc059fa4eb535bd
Pitchapong Charmtong <pitchapongcharmtong@cocoabeans-MacBook-Pro.local>
2023-02-27 23:01:13 +0700

    first commit

---
+++ q13.ts
@@ -0,0 +1,14 @@
+// 13.Please write a typescript project with unit test:
+
+// For implementing function with this interface
+//     merge (int[] collection_1, int[] collection_2) : int []
+
+// given
+//      collection_1, collection_2 already sorted from min(0) to max
+
+// Please provide accessible repo of typescript project with unit test
+// and provide how to setup dependency and execute code and unit test
+
+// Note: Don't allow to use any sort function
+
+
